5375985b3fd03ecd69f8d9d8db6a8d442b2ae88f
MikailOzturk <michael.ztrk@gmail.com>
2023-11-20 19:53:16 +0300

    next commit

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Image, Text, StyleSheet, SafeAreaView, 
+  TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState  } from 'react'
 import Voice from '@react-native-voice/voice'
 
 const App = () => {
+  const [result, setResult] = useState('')
+  const [isLoading, setLoading] = useState(false)
+
   useEffect(()=>{
     Voice.onSpeechStart = onSpeechStart;
     Voice.onSpeechEnd = onSpeechEnd;
@@ -14,20 +18,68 @@ const App = () => {
   },[])
 
   const onSpeechStart = (e) => {
-    console.log(e)
+    console.log("start handler==>>>", e)
   }
   const onSpeechEnd = (e) => {
-    console.log(e)
+    setLoading(false)
+    console.log("stop handler", e)
   }
   const onSpeechResults = (e) => {
-    console.log(e)
+    let text = e.value[0]
+    setResult(text)
+    console.log("speech result handler", e)
+  }
+
+  const startRecording = async () => {
+    setLoading(true)
+    try {
+      await Voice.start('en-Us')
+    } catch (error) {
+      console.log("error raised", error)
+    }
+  }
+  const stopRecording = async () => {
+    try {
+      await Voice.stop()
+    } catch (error) {
+      console.log("error raised", error)
+    }
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.headingText}>Demo</Text>
-      <TouchableOpacity style={styles.mic}>
-        <Image source={require('./button.png')}/>
-      </TouchableOpacity>
+      <SafeAreaView>
+        <Text style={styles.headingText}>Speech Recoginition</Text>
+        <View style={styles.textInputStyle}>
+          <TextInput
+            value={result}
+            placeholder="your text"
+            style={{ flex: 1 }}
+            onChangeText={text => setResult(text)}
+          />
+          {isLoading ? <ActivityIndicator size="large" color="red" />
+
+            :
+            
+            <TouchableOpacity
+              onPress={startRecording}
+            >
+              <Image
+                source={{ uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/microphone.png' }}
+                style={{ width: 25, height: 25 }}
+              />
+            </TouchableOpacity>}
+        </View>
+
+        <TouchableOpacity
+          style={styles.stopRecord}
+          onPress={stopRecording}
+        >
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>Stop</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      
+   
+ 
     </View>
   )
 }
@@ -47,10 +99,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 26
   },
-  mic: {
-    alignSelf: 'center',
-    marginTop: 50,
-  },
   textInputStyle: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -63,5 +111,12 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     elevation: 2,
     shadowOpacity: 0.4
+  },
+  stopRecord: {
+    alignSelf: 'center',
+    marginTop: 24,
+    backgroundColor: 'red',
+    padding: 8,
+    borderRadius: 4
   }
 });
